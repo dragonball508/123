419de5081e854f3cccf4a0002244b393a446f35d
dragonballz508 <wilimer.castro@ticxar.com>
2020-02-12 11:33:04 -0500

    prueba3

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/Desktop/2/ipfood'));
+app.use(express.static(__dirname + '/dist/ipfood'));
 
-app.get('*', function(req,res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname+ '/dist/Desktop/2/ipfood/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/ipfood/index.html'));
 });
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
